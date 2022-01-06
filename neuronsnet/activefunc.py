aaec9b118f8414c2ae543bc2df48b13f952cbf39
wu tao <codeandmore@126.com>
2022-01-06 15:01:27 +0800

    根delta 导数矩阵

--- neuronsnet/activefunc.py
+++ neuronsnet/activefunc.py
@@ -16,3 +16,6 @@ def sigmoid_func_derivative(a: float) -> float:
 
 def loss_4_sigmoid(y_expected: float, y_pred: float) -> float:
     pass
+# https://zhuanlan.zhihu.com/p/79657669
+# 对于回归问题，对out直接计算损失，损失函数为MSE
+# 对于分类问题，out后接softmax进行分类，然后使用CE(cross entropy)计算loss.
--- neuronsnet/nn.py
+++ neuronsnet/nn.py
@@ -11,6 +11,7 @@ from neuronsnet import activefunc
 
 ActiveFunction = Callable[[List[float]], List[float]]
 DerivativeActiveFunction = Callable[[List[float]], List[float]]
+Loss = Callable[[List[float], List[float]], List[float]]
 
 # def active_func(Z: List[float]) -> List[float]:
 #     result: List[float] = []
@@ -37,37 +38,40 @@ class Layer:
             weights * nodes).reshape(weights, nodes)
         self._B: np.ndarray = np.random.rand(nodes)
 
-        self._Active_Func = active_func
-        self._Derivative_Active = derivative_active
+        self._active_func = active_func
+        self._derivative_fctive = derivative_active
 
         self._Z: np.ndarray = None
         self._A: np.ndarray = None
 
-        self._X_input: np.ndarray = None
-        pass
+        self._X_input: np.matrix = None
 
     def calc_z(self, X: List[float]) -> List[float]:
-        self._X_input = np.array(X)
-        self._Z = self._X_input.dot(self._W) + self._B
+        self._X_input = np.mat(X)
+        self._Z = np.dot(self._X_input, self._W) + self._B
         return self._Z.tolist()
 
     def calc_a(self) -> List[float]:
-        resultset: List[float] = self._Active_Func(self._Z.tolist())
+        resultset: List[float] = self._active_func(self._Z.tolist())
         self._A = np.array(resultset)
         return resultset
 
     def gradient_descent(self, delta_next_layer: List[float], weights_next_layer: List[List[float]],
-                         learn_rate: float) -> Tuple(List[float], List[List[float]]):
+                         learn_rate: float) -> Tuple[List[float], List[List[float]]]:
 
         DNL: np.matrix = np.mat(delta_next_layer)
         WNL: np.matrix = np.mat(weights_next_layer)
-        # np.dot(DNL, WNL.T)* 激活函数导数的矩阵
+        # TODO np.dot(DNL, WNL.T)* 激活函数导数的矩阵
         DCL: np.matrix = NoneType
 
-        delta_cur: List[float] = DCL.tolist()[0]
-        delta_weigts:  np.matrix
-        delta_b: np.ndarray
-        return delta_cur, self._W
+        ret: Tuple[List[float], List[List[float]]] = tuple(DCL.tolist()[0],
+                                                           self._W.tolist())
+
+        delta_weigts: np.matrix = np.dot(self._X_input.T, DCL)
+        delta_b: np.ndarray = DCL.A[0]
+        self._W = self._W - learn_rate * delta_weigts
+        self._B = self._B - learn_rate * delta_b
+        return ret
 
     def nodes(self) -> int:
         return self._w_shape[1]
@@ -76,18 +80,12 @@ class Layer:
 Decimal = TypeVar("Decimal", bound=(int, float))
 
 
-def generate_eye_matrix(n: int,   v: Decimal) -> List[List[Decimal]]:
+def generate_eye_matrix(n: int, v: Decimal) -> List[List[Decimal]]:
     matrix: List[List[Decimal]] = []
     for i in range(n):
         row: List[Decimal] = [v if c == i else 0 for c in range(n)]
         matrix.append(row)
     return matrix
-
-
-# Trainable = TypeVar('Trainable')
-# TagsMapper = Callable[[Trainable], Tuple[List[float], float]]
-# ''' object => (X[x1,x2,x3,...],y)'''
-Loss = Callable[[List[float], List[float]], List[float]]
 
 
 class NeuronsNetFramework:
@@ -98,19 +96,19 @@ class NeuronsNetFramework:
                                          ActiveFunction, DerivativeActiveFunction]] = layers
         self._loss: Loss = loss
         self._layers: List[Layer] = []
-        pass
 
-    def train(self, X: List[List[float]], Y: List[List[float]], input_nodes: int, epoches: int, learn_rate: float):
-        self._layers_setting.insert(0, (input_nodes, None, None))
+    def train(self, X: List[List[float]], Y: List[List[float]], epoches: int, learn_rate: float):
+        self._layers_setting.insert(0, (len(X[0]), None, None))
         for i in range(1, len(self._layers_setting), 1):
-            _layer: Tuple(int, ActiveFunction,
-                          DerivativeActiveFunction) = self._layers_setting[i]
-            _pre_layer: Tuple(int, ActiveFunction,
-                              DerivativeActiveFunction) = self._layers_setting[i-1]
-            nodes = _layer[0]
-            weights = _pre_layer[0]
-            self._layers.append(Layer(nodes=nodes, weights=weights,
-                                      active_func=_layer[1], derivative_active=_layer[2]))
+            layer_setting: Tuple(int, ActiveFunction,
+                                 DerivativeActiveFunction) = self._layers_setting[i]
+            pre_layer_setting: Tuple(int, ActiveFunction,
+                                     DerivativeActiveFunction) = self._layers_setting[i-1]
+            nodes = layer_setting[0]
+            weights = pre_layer_setting[0]
+            layer: Layer = Layer(nodes=nodes, weights=weights,
+                                 active_func=layer_setting[1], derivative_active=layer_setting[2])
+            self._layers.append(layer)
         self._X = X
         self._Y = Y
 
@@ -119,9 +117,8 @@ class NeuronsNetFramework:
                 y_pred: List[float] = self._forward(x_sample)
                 loss_delta: List[float] = self._cost(y_expect, y_pred)
                 self._back(loss_delta, learn_rate)
-
-            print("in epoches {} loss {}".format(e))
-        pass
+            # TODO 算一下当前权重下的 损失
+            print("in epoches {} loss {}".format(e, 0))
 
     def _forward(self, x: List[float]) -> List[float]:
         _input_x: List[float] = x
@@ -132,7 +129,6 @@ class NeuronsNetFramework:
 
     def _cost(self, y_expect: List[float], y_pred: List[float]) -> List[float]:
         self._loss
-        pass
 
     def _back(self, delta: List[float], learn_rate: float):
 
@@ -142,8 +138,8 @@ class NeuronsNetFramework:
         weights_next_layer: List[List[float]] = virtual_w
         delta_next_layer: List[float] = delta
         for l in self._layers[:0:-1]:
-            delta_next_layer, weights_next_layer = l.gradient_descent(delta_next_layer,
-                                                                      weights_next_layer)
+            delta_next_layer, weights_next_layer = l.gradient_descent(delta_next_layer, weights_next_layer,
+                                                                      learn_rate)
 
 
 l = Layer(3, 4)
