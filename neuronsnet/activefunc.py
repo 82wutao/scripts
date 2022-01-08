49b7d79a33caa8a8305f5cafaffaaf94588f0015
wu tao <82wutao@gmail.com>
2022-01-08 21:50:12 +0800

    sigmoid,softmax as active func & derivative func and mse,crossentropy as loss func & derivative func

--- neuronsnet/activefunc.py
+++ neuronsnet/activefunc.py
@@ -1,28 +1,46 @@
 
 import math
-from typing import Callable, List
+from typing import Callable, Dict, List, NoReturn
 import numpy as np
+import functools as util_funcs
 
 
-def sigmoid(z: float) -> float:
+def sigmoid(Z: List[float]) -> float:
     ''' 1/(1 + e^{-x})'''
-    p: float = 1 + math.exp(-z)
-    return 1/p
+    ret: List[float] = [1/(1 + math.exp(-z)) for z in Z]
+    return ret
 
 
-def sigmoid_derivative(a: float) -> float:
+def sigmoid_derivative(A: List[float]) -> float:
     '''f(a)*(1-f(a))'''
-    f: Callable[[float], float] = sigmoid
-    return f(a)*(1-f(a))
+
+    Z: List[float] = sigmoid(A)
+    return [z * (1-z)for z in Z]
 
 
 def softmax(Y: List[float]) -> List[float]:
     '''激活函数'''
-    s: float = 0
-    for y in Y:
-        s = sum + math.exp(y)
+    a: float = max(Y)
 
-    return [math.exp(i)/s for i in Y]
+    s: float = 0
+    exp_yi: List[float] = []
+    for yi in Y:
+        r: float = math.exp(yi-a)
+        s = s + r
+        exp_yi.append(r)
+
+    return [expi/s for expi in exp_yi]
+
+
+def softmax_derivative(y_expected: List[float], Y_pred: List[float]) -> List[float]:
+    '''
+    激活函数偏导数，求delta: softmax偏导数\n
+    每个xi的 softmax 概率输出都相对yexp =1 的节点概率误差求偏导数
+    '''
+    return [util_funcs.reduce(lambda s, x:s+x,
+                              [Y_pred[i]*(1-Y_pred[j]) if j == i else -(Y_pred[j] * Y_pred[i])
+                               for i in range(len(Y_pred)) if y_expected[i] > 0], 0)
+            for j in range(len(Y_pred))]
 
 
 def meansquarederror(y_expected: List[float], y_pred: List[float]) -> float:
@@ -40,6 +58,30 @@ def meansquarederror_derivative(y_expected: List[float], y_pred: List[float]) ->
     diff = yp-ye
     return np.sum(diff*2)/len(y_expected)
 
-# https://zhuanlan.zhihu.com/p/79657669
-# 对于回归问题，对out直接计算损失，损失函数为MSE
-# 对于分类问题，out后接softmax进行分类，然后使用CE(cross entropy)计算loss.
+
+def crossentropy(y_expected: List[float], y_pred: List[float]) -> float:
+    '''损失函数: 交叉熵'''
+    cost: float = util_funcs.reduce(lambda s, x: -(x[0] * math.log(x[1]))+s,
+                                    [expected_pred for expected_pred in zip(y_expected, y_pred) if expected_pred[0] > 0], 0)
+    return cost
+
+
+def crossentropy_derivative(y_expected: List[float], y_pred: List[float]) -> List[float]:
+    '''损失函数偏导数，求delta: 交叉熵偏导数。每个softmax 的概率输出的偏导数都一样'''
+    delta: float = util_funcs.reduce(lambda s, x: -(x[0]/x[1]) + s,
+                                     [expected_pred for expected_pred in zip(y_expected, y_pred) if expected_pred[0] > 0], 0)
+    ret: List[float] = [delta for i in range(len(y_pred))]
+    return ret
+
+
+s_r = softmax([2, 3, 4])
+print("softmax {}".format(s_r))
+
+sd_r = softmax_derivative([0, 1, 0], s_r)
+print("softmax_derivative {}".format(sd_r))
+# print(softmax_derivative([4, -4, 3]))
+
+# print("loss {}".format(crossentropy([0, 1, 0], s_r)))
+cd_r = crossentropy_derivative([0, 1, 0], s_r)
+print("cd_r {}".format(cd_r))
+print([z[0] * z[1] for z in zip(sd_r, cd_r)])
